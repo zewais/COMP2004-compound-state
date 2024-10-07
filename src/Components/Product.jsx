fdcb2f6793182cb9f7a1602dfe9335f53594b38f
zewais <arch.ziad.ewais@gmail.com>
2024-10-07 09:16:01 -0400

    add dropdown menu for price

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,13 +1,52 @@
+//Product component takes in four props: img, name, quantity, and priceOptions.
+//img is a string that represents the URL of the image of the product.
+//name is a string that represents the name of the product.
+//quantity is a number that represents the quantity of the product.
+//priceOptions is an array of numbers that represents the different price options for the product.
+//Product component renders the image, name, quantity, price, total price, and two buttons to add or remove the product.
+//The price is displayed as a dropdown menu with the different price options.
+
 import { useState } from "react";
-export default function Product({ img, name, quantity, price }) {
-  const [quantityCount, setQuantityCount] = useState({ quantity });
+
+export default function Product({ img, name, quantity, priceOptions }) {
+  //quantityCount is an object with two properties: quantity and priceOptions.
+  //quantityCount is initialized with the quantity and the first price option.
+  //quantityCount is a state variable that is updated using the setQuantityCount function.
+  const [quantityCount, setQuantityCount] = useState({
+    quantity,
+    priceOptions: priceOptions[0],
+  });
   return (
     <div className="product">
       <img src={img} alt={name} height="100px" />
       <h3>{name}</h3>
       <p>Quantity: {quantityCount.quantity}</p>
-      <p>Price: {price}</p>
-      <p>Total Price: {(price * quantityCount.quantity).toFixed(2)}</p>
+      <p>
+        Price:{" "}
+        {
+          <select
+            value={quantityCount.priceOptions}
+            onChange={(e) =>
+              setQuantityCount((prevCount) => {
+                return {
+                  ...prevCount,
+                  priceOptions: parseFloat(e.target.value),
+                };
+              })
+            }
+          >
+            {priceOptions.map((price, index) => (
+              <option key={index} value={price}>
+                {price.toFixed(2)}
+              </option>
+            ))}
+          </select>
+        }
+      </p>
+      <p>
+        Total Price:{" "}
+        {(quantityCount.priceOptions * quantityCount.quantity).toFixed(2)}
+      </p>
       <button
         onClick={() =>
           setQuantityCount((prevCount) => {
--- src/Components/ProductsContainer.jsx
+++ src/Components/ProductsContainer.jsx
@@ -1,3 +1,6 @@
+// This component is responsible for rendering the list of products.
+// The list of products is passed as a prop to the ProductsContainer component.
+// The ProductsContainer component maps over the list of products and renders each product using the Product component.
 import Product from "./Product";
 export default function ProductsContainer({ products }) {
   return (
